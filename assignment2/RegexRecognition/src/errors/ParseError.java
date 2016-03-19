84ae57f32e376e18ba289991590932352c387ee5
Florian Thuin <florian.thuin@student.uclouvain.be>
2016-03-19 16:42:18 +0100

    update RegexRecognition + Rapport

---
+++ assignment2/RegexRecognition/src/errors/ParseError.java
@@ -0,0 +1,10 @@
+package errors;
+
+public class ParseError extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ParseError() {
+		super("Parse error !");
+	}
+} 
---
+++ assignment2/RegexRecognition/src/regex/Regex.java
@@ -0,0 +1,12 @@
+package regex;
+
+public class Regex {
+	
+	//modify this 
+	public boolean parse(String[] input){
+		
+		return true;
+	}
+	
+	
+}
